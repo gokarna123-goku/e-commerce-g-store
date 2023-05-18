6c4c41fc447087a83890f0a87c05385ab08ce122
gokarna123-goku <agokarna444@gmail.com>
2023-05-18 15:21:58 +0545

    fixing the issues

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-// 
+//
 window.addEventListener("scroll", function () {
-    var navbar = document.querySelector(".logo_search_section");
-    var scrollY = window.scrollY;
-  
-    if (scrollY >= 50) {
-      navbar.classList.add("fixed");
-    } else {
-      navbar.classList.remove("fixed");
-    }
-  });
-  
+  var navbar = document.querySelector(".logo_search_section");
+  var scrollY = window.scrollY;
 
+  if (scrollY >= 50) {
+    navbar.classList.add("fixed");
+  } else {
+    navbar.classList.remove("fixed");
+  }
+});
 
 // End of JavaScript
