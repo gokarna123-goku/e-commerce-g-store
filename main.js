bf423265a5b3e98b8fe1bee97a16ea5b964776f5
gokarna123-goku <agokarna444@gmail.com>
2023-05-18 15:09:07 +0545

    added javascript code

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 // 
+window.addEventListener("scroll", function () {
+    var navbar = document.querySelector(".logo_search_section");
+    var scrollY = window.scrollY;
+  
+    if (scrollY >= 50) {
+      navbar.classList.add("fixed");
+    } else {
+      navbar.classList.remove("fixed");
+    }
+  });
+  
 
 
 // End of JavaScript
