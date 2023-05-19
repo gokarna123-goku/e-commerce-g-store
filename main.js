5943842ec9c3f8b78caf22e6fd9b034a094f60b0
gokarna123-goku <agokarna444@gmail.com>
2023-05-19 10:32:17 +0545

    changes the things

--- main.js
+++ main.js
@@ -32,4 +32,38 @@ $(document).ready(() => {
   });
 });
 
+
+// Notification Toggle
+// const notifyButton = document.getElementById("notify_toggle");
+// const notifyIcon = document.getElementById("notify_icon");
+// const notifyBox = document.getElementById("notification_box");
+
+// notifyButton.addEventListener("click", () => {
+//   if (notifyBox.style.right === "-100%") {
+//     notifyBox.style.right = "1%";
+//     notifyButton.style.backgroundColor = "#dedede";
+//     notifyIcon.style.color = "#f14c05";
+//     // notifyButton.classList.add("notification_active");
+//   } else {
+//     notifyBox.style.right = "-100%";
+//     notifyButton.style.backgroundColor = "#fafafa";
+//     notifyIcon.style.color = "#333333";
+//     // notifyButton.classList.remove("notification_active");
+//   }
+// });
+
+// Profile Toggle Section
+const profileButton = document.getElementById("profile_toggle");
+const profileBox = document.getElementById("profile_box");
+
+profileButton.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (profileBox.style.right === "-100%") {
+    profileBox.style.right = "1%";
+  } else {
+    profileBox.style.right = "-100%";
+  }
+});
+
+
 // End of JavaScript
